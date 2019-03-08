f404a693a7716cd92c5a0d2ae551077e419231f2
leeli <leeli@leelis-iMac-2.local>
2019-03-08 20:48:51 +0800

    add rspec routing test

---
+++ spec/routing/pages_routing_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "routes for Pages", :type => :routing do
+
+  context "routes to the pages controller" do
+    it "routes /order_dvd to the pages controller" do
+      expect(get("/order_dvd")).to route_to("controller"=>"pages", "action"=>"show", "path"=>"order_dvd")
+    end
+
+    it "routes /order_dvd_2018 to the pages controller" do
+      expect(get("/order_dvd_2018")).to route_to("controller"=>"pages", "action"=>"show", "path"=>"order_dvd_2018")
+    end
+  end
+
+  context "routeable to the pages controller" do
+    
+  end
+end
